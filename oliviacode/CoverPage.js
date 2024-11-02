381c50c4ffea08df59d95aabc528532a8a6bf148
Olivia Liu <oaliu@unc.edu>
2024-11-02 17:42:18 -0400

    Add files via upload

---
+++ oliviacode/CoverPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './CoverPage.css';
+
+function CoverPage() {
+  return (
+    <div className="cover-page">
+      <h1 className="cover-title">What's the Move?</h1>
+      <button className="cover-button">Get started</button>
+    </div>
+  );
+}
+
+export default CoverPage;
